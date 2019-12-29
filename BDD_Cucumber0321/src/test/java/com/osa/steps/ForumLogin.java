d70d681f937cd4153f34c48a8155b0f6623e25c3
Md Afur Uddin <58801976+afuruddin@users.noreply.github.com>
2019-12-29 16:49:22 -0500

    Adding Utility method & log4j dependency

--- BDD_Cucumber0321/src/test/java/com/osa/steps/ForumLogin.java
+++ BDD_Cucumber0321/src/test/java/com/osa/steps/ForumLogin.java
@@ -1,5 +1,6 @@
 package com.osa.steps;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
 import com.osa.base.Browser;
@@ -14,11 +15,14 @@ public class ForumLogin {
 	HomePagesFactory hp;
 	ForumLoginPageFactory fl;
 	WebDriver dr;
+	public static Logger log=Utility.getLog(ForumLogin.class);
 	@Given("^I am on Forum login page$")
 	public void i_am_on_Forum_login_page() throws Throwable {
 	  dr=Browser.openBrowser();
+	  log.info("Navitaging to https://www.osaconsultingtech.com");
 	  dr.get("https://www.osaconsultingtech.com");
 	  hp=new HomePagesFactory(dr);
+	  log.info("Clicking on Forum login button");
 	  hp.clickOnForumLoginButton();
 	}
 
@@ -31,12 +35,14 @@ public class ForumLogin {
 
 	@Then("^I verify the login is successful$")
 	public void i_verify_the_login_is_successful() throws Throwable {
+	  log.info("Clicking on login button");
 	  fl.clickOnLoginButton();
 	  
 	}
 	@And("^I close the browser$")
 	public void i_close_the_browser() throws InterruptedException {
 		Thread.sleep(5000);
+		log.info("Closing Browser");
 		dr.close();
 	}
 }
